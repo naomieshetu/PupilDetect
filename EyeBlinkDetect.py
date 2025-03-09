744463d178a04eeddbf90ebaaae6f0748235b47f
naomieshetu <124212130+naomieshetu@users.noreply.github.com>
2025-03-09 14:36:48 -0400

    Add files via upload

--- EyeBlinkDetect.py
+++ EyeBlinkDetect.py
@@ -1,3 +1,4 @@
+import numpy as np      # Will use this for pupil detection
 import cv2              # For video rendering 
 import dlib             # For face and landmark detection 
 import imutils
@@ -15,7 +16,7 @@ cam = cv2.VideoCapture(0)                          # To use live web cam
 def calculate_EAR(eye):                            # Calculate the eye aspect ratio
       
     # calculate the vertical distances between upper and lower eyelid points
-    # euclidean distance is like calculating the hypotenuse in a right triangle 
+    # euclidean distance -> calculating the hypotenuse in a right triangle 
     y1 = dist.euclidean(eye[1], eye[5]) 
     y2 = dist.euclidean(eye[2], eye[4]) 
   
@@ -28,8 +29,8 @@ def calculate_EAR(eye):                            # Calculate the eye aspect ra
     return EAR
 
 # Variables
-blink_thresh = 0.45        # Threshold EAR value - if below eye is considered closed
-succ_frame = 2             # Number of consecutive frames eye must be closed before it's counted as a blink
+blink_thresh = 0.35        # Threshold EAR value
+succ_frame = 2             # Number of consecutive frames (eye must be closed before it's counted as a blink)
 count_frame = 0
 
 # Eye landmarks 
@@ -41,12 +42,11 @@ detector = dlib.get_frontal_face_detector()
 
 # Construct absolute path
 model_path = os.path.join(os.getcwd(), "Model", "shape_predictor_68_face_landmarks.dat")
-#Load the model
 landmark_predict = dlib.shape_predictor(model_path)
+
 while 1: 
   
-    # If the video is finished then reset it 
-    # to the start 
+     
     if cam.get(cv2.CAP_PROP_POS_FRAMES) == cam.get(cv2.CAP_PROP_FRAME_COUNT): 
         cam.set(cv2.CAP_PROP_POS_FRAMES, 0) 
   
@@ -64,7 +64,6 @@ while 1:
   
             # landmark detection 
             shape = landmark_predict(img_gray, face)
-            #shape = landmark_predict(img_gray, dlib.rectangle(face.left(), face.top(), face.right(), face.bottom())) 
   
             # converting the shape class directly to a list of (x,y) coordinates 
             shape = face_utils.shape_to_np(shape) 
@@ -81,7 +80,7 @@ while 1:
             avg = (left_EAR+right_EAR)/2
 
             if avg < blink_thresh: 
-                count_frame += 1  # incrementing the frame count 
+                count_frame += 1  # Incrementing the frame count 
             else: 
                 if count_frame >= succ_frame: 
                     cv2.putText(frame, 'Blink Detected', (30, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 1) 
